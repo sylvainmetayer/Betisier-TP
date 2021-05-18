1a3e4fc28f30e285ad648862372dffcb2c714295
Sylvain METAYER <github@sylvainmetayer.fr>
2021-05-18 13:46:53 +0200

    test cloudflare analytics

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -4,5 +4,9 @@ Le bêtisier de l'IUT à votre service, depuis novembre 2013
     © IUT du Limousin  DUT Informatique année 2. 
 </div>
 
+<!-- Cloudflare Web Analytics -->
+<script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "6d062a42f021427f8066a60c2aecdc77"}'></script>
+<!-- End Cloudflare Web Analytics -->
+
 </body>
 </html>
